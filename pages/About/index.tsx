24d2860eb25b73c3cc2c8ed7ef9605b77ff54a7d
Célestin Meunier <hello@celest.in>
2022-09-29 17:26:26 +0200

    added anout section

--- pages/About/index.tsx
+++ pages/About/index.tsx
@@ -1,11 +1,21 @@
 import RotatingHead from "../../components/background/rotatingHead/RotatingHead";
 import UnderConstruction from "../../components/tools/UnderConstruction";
 
+
+
 const About = () => {
 	return (  
 		<div className="w-full h-full">
 			<RotatingHead/>
-			{/* <UnderConstruction alternateRessourceName="Github" alternateRessourceLink="https://github.com/backslash-zero"/> */}
+			<div className="absolute w-full h-full 
+							flex flex-col items-center justify-center
+							p-4
+							font-mono text-stone-100 leading-8">
+				<p>I&apos;m Célestin, <br/>
+				a Lyon-born, Berlin-based, Designer, Developer and Media Artist. <br/> 
+				I like to work with code, 3D, video, and photography.<br/>
+				Contact: hello (at) celest.in</p>
+			</div>
 		</div>
 	);
 }
