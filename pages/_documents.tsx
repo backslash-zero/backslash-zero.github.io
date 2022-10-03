5bc5373c807c619081e4350d2502650f7c6b62da
Célestin Meunier <hello@celest.in>
2022-10-03 23:29:16 +0200

    updated fonts

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -1,26 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx : any) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-			<link 
-				href="http://fonts.cdnfonts.com/css/jetbrains-mono" 
-				rel="stylesheet"/>
-		</Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+	return (
+		<Html>
+		<Head>
+			<link
+					href="http://fonts.cdnfonts.com/css/jetbrains-mono" 
+					rel="stylesheet"/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
-
-export default MyDocument
