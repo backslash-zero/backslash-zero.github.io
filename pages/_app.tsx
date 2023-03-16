f9c23c131e8f2e9170627bfff9423e07225e8531
Celestin <hello@celest.in>
2023-03-16 18:19:09 +0100

    removed frontend cv

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,12 +18,12 @@ const ibmPlexMono = IBM_Plex_Mono({
 const Avara = localFont({
   src: [
     {
-      path: "fonts/Avara/Avara.woff2",
+      path: "./fonts/Avara/Avara.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "fonts/Avara/Avara-italic.woff2",
+      path: "./fonts/Avara/Avara-italic.woff2",
       weight: "400",
       style: "italic",
     },
@@ -34,82 +34,82 @@ const suisseIntl = localFont({
   src: [
     // Suisse Intl with all weights and styles
     {
-      path: "fonts/Suisse/SuisseIntl-UltraLight-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-UltraLight-WebS.woff2",
       weight: "100",
       style: "normal",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-UltraLightItalic-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-UltraLightItalic-WebS.woff2",
       weight: "100",
       style: "italic",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-Light-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-Light-WebS.woff2",
       weight: "200",
       style: "normal",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-LightItalic-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-LightItalic-WebS.woff2",
       weight: "200",
       style: "italic",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-Book-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-Book-WebS.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-BookItalic-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-BookItalic-WebS.woff2",
       weight: "300",
       style: "italic",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-Regular-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-Regular-WebS.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-RegularItalic-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-RegularItalic-WebS.woff2",
       weight: "400",
       style: "italic",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-Medium-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-Medium-WebS.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-MediumItalic-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-MediumItalic-WebS.woff2",
       weight: "500",
       style: "italic",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-SemiBold-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-SemiBold-WebS.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-SemiBoldItalic-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-SemiBoldItalic-WebS.woff2",
       weight: "600",
       style: "italic",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-Bold-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-Bold-WebS.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-BoldItalic-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-BoldItalic-WebS.woff2",
       weight: "700",
       style: "italic",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-Black-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-Black-WebS.woff2",
       weight: "900",
       style: "normal",
     },
     {
-      path: "fonts/Suisse/SuisseIntl-BlackItalic-WebS.woff2",
+      path: "./fonts/Suisse/SuisseIntl-BlackItalic-WebS.woff2",
       weight: "900",
       style: "italic",
     },
